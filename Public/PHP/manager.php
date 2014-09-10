dc6dbe0414916ddaa2d8ecc61ce3b8bc69a7faec
Dharmakeerthi Lasantha <tcdlpds@gmail.com>
2014-09-11 04:15:50 +0530

    Manager php file changed
    manager php file changed and add member method developed

--- Public/PHP/manager.php
+++ Public/PHP/manager.php
@@ -1,62 +1,98 @@
 <?php
-    include("../../Includes/Header.php");
+include("../../Includes/Header.php");
 ?>
 <body>
 
 <div class="container" id="main">
     <div class="navbar navbar-fixed-top">
-        <div class="container">
-            <button class="navbar-toggle" data-target=".navbar-responsive-collapse" data-toggle="collapse" type="button">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class = "navbar-brand" href="#"><img src = "../../Images/logo.png" alt="My Logo"></a>
-            <div class = "nav-collapse collapse navbar-responsive-collapse">
-                <ul class = "nav navbar-nav">
-                    <li><a href = "manager main.php">Home</a></li>
-                    <li class= "active"><a href = "#">Manage Profiles</a></li>
-                    <li class="dropdown">
-                        <a href = "#" class="dropdown-toggle" data-toggle="dropdown">Reports<strong class="caret"></strong></a>
-                        <ul class = "dropdown-menu">
-                            <li><a href = "#">View Report</a></li>
-                            <li><a href = "#">Send Report</a></li>
-                        </ul><!--end of dropdown menu-->
-                    </li>
-                </ul>
-                <ul class = "nav navbar-nav pull-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"> Manager<strong class="caret"></strong></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#"><span class="glyphicon glyphicon-cog"> Settings</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-briefcase"> Update Profile</a></li>
-                            <li class="divider"></li>
-                            <li><a href="../../index.html"><span class="glyphicon glyphicon-off"> Sign-Out</a></li><!--methenta home eka link karahan-->
-                        </ul>
+        <a class = "navbar-brand" href="#"><img src = "../../Images/logo.png" alt="My Logo"></a>
+        <div class = "nav-collapse collapse navbar-responsive-collapse">
+            <ul class = "nav navbar-nav pull-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"> Manager<strong class="caret"></strong></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><span class="glyphicon glyphicon-cog"> Settings</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-briefcase"> Update Profile</a></li>
+                        <li class="divider"></li>
+                        <li><a href="../../index.html"><span class="glyphicon glyphicon-off"> Sign-Out</a></li><!--methenta home eka link karahan-->
+                    </ul>
 
-                    </li>
-                </ul>
-            </div><!--end nav-collapse-->
-        </div><!--end container-->
+                </li>
+            </ul>
+        </div><!--end nav-collapse-->
+    </div><!--end container-->
 
-    </div><!--end navbar-->
+</div><!--end navbar-->
+<hr>
+<hr>
+<hr>
+<div class="row" id="row1">
+    <div class="col-sm-3 feature">
+        <div class="panel">
+            <div class= "panel-heading">
+                <h3 class="panel-title">Supliers</h3>
+            </div>
+            <img src="../../Images/supplier.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a class="btn btn-default btn-block">View Details</a>
+                <a class="btn btn-default btn-block">Add Supplier</a>
+            </div>
+        </div>
+    </div>
 
-</div> <!--end container-->
+    <div class="col-sm-3 feature">
+        <div class="panel">
+            <div class= "panel-heading">
+                <h3 class="panel-title">Reports</h3>
+            </div>
+            <img src="../../Images/report.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a class="btn btn-default btn-block">Daily Report</a>
+                <a class="btn btn-default btn-block">Monthly Report</a>
+            </div>
+        </div>
+    </div>
 
-<div>
-    <hr>
-    <hr>
-    <h3 id="stfmember">STAFF</h3>
+    <div class="col-sm-3 feature">
+        <div class="panel">
+            <div class= "panel-heading">
+                <h3 class="panel-title">Book Stock</h3>
+            </div>
+            <img src="../../Images/book.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a class="btn btn-default btn-block">Book Details</a>
+                <a class="btn btn-default btn-block">Sales</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-3 feature">
+        <div class="panel">
+            <div class= "panel-heading">
+                <h3 class="panel-title">Payables & Receivables</h3>
+            </div>
+            <img src="../../Images/payables.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a class="btn btn-default btn-block">Payables</a>
+                <a class="btn btn-default btn-block">Receivables</a>
+            </div>
+        </div>
+    </div>
 </div>
+
+<hr>
+
 <div class="row" id="staffDetails">
     <div class="col-sm-3 feature">
         <div class="panel">
             <div class= "panel-heading">
                 <h3 class="panel-title">Chairman</h3>
             </div>
-            <img src="../../Images/chairman.jpg" alt="HTML5" class="img-circle">
-            <a class="btn btn-default btn-block">View Details</a>
+            <img src="../../Images/chairman.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a class="btn btn-default btn-block">View Details</a>
+                <a class="btn btn-default btn-block">Delete Profile</a>
+            </div>
         </div>
     </div>
 
@@ -65,8 +101,11 @@
             <div class= "panel-heading">
                 <h3 class="panel-title">Manager</h3>
             </div>
-            <img src="../../Images/Manager.jpg" alt="HTML5" class="img-circle">
-            <a class="btn btn-default btn-block">View Details</a>
+            <img src="../../Images/Manager.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a class="btn btn-default btn-block">View Details</a>
+                <a class="btn btn-default btn-block">Delete Profile</a>
+            </div>
         </div>
     </div>
 
@@ -75,8 +114,11 @@
             <div class= "panel-heading">
                 <h3 class="panel-title">Stock Keeper</h3>
             </div>
-            <img src="../../Images/Stock Keeper.jpg" alt="HTML5" class="img-circle">
-            <a class="btn btn-default btn-block">View Details</a>
+            <img src="../../Images/Stock Keeper.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a class="btn btn-default btn-block">View Details</a>
+                <a class="btn btn-default btn-block">Delete Profile</a>
+            </div>
         </div>
     </div>
 
@@ -85,112 +127,115 @@
             <div class= "panel-heading">
                 <h3 class="panel-title">Cashier</h3>
             </div>
-            <img src="../../Images/Cashier.jpg" alt="HTML5" class="img-circle">
-            <a class="btn btn-default btn-block">View Details</a>
+            <img src="../../Images/Cashier.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a class="btn btn-default btn-block">View Details</a>
+                <a class="btn btn-default btn-block">Delete Profile</a>
+            </div>
         </div>
     </div>
 </div>
 
-<div>
-    <div class="row" id="btnMenu">
-        <div class="col-sm-4 feature">
-            <a href="#addProfile" role="button" class="btn btn-info" data-toggle="modal" id="addButton">ADD PROFILE</a>
-            <div class="modal fade" id="addProfile">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button class ="close" data-dismiss="modal">&times;</button>
-                            <h5 class="modal-title">Add Profile</h5>
-                        </div>
-
-                        <div class="modal-body">
-                            <h6>Employee Details Form</h6><!--mekata data base eka link ka balahan-->
-                            <form class="form-horizontal" action="SignUpFormProcessing.php" method="post" name="reg" onsubmit="return checkForm();">
-
-                                <div class="form-group">
-                                    <label class="col-lg-2 control-label" for="first_name">First Name</label>
-                                    <div class="col-lg-10">
-                                        <input class="form-control" id="Name" placeholder="First Name" type="text" required name="firstName">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-2 control-label" for="Last_name">Last Name</label>
-                                    <div class="col-lg-10">
-                                        <input class="form-control" id="Name" placeholder="Last Name" type="text" required name="lastName">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-2 control-label" for="Birth_Day">Birth Day</label>
-                                    <div class="col-lg-10">
-                                        <input class="form-control" type="date" placeholder="Birth Day" name="bDay">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-2 control-label" for="Username">UserName</label>
-                                    <div class="col-lg-4">
-                                        <input class="form-control" id="Name" placeholder="UserName" type="text" name="username" required>
-                                    </div>
-
-                                    <label class="col-lg-2 control-label" for="Password">Password</label>
-                                    <div class="col-lg-4">
-                                        <input class="form-control" id="passwordName" placeholder="Password" type="password" name="passwordName" required>
-                                    </div>
-                                </div>
-
-
-
-                                <div class="form-group">
-
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-2 control-label" for="Confirm_password">Confirm password</label>
-                                    <div class="col-lg-4">
-                                        <input class="form-control" id="confirmPassword" placeholder="Confirm password" type="password" required name="confirmPassword">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="col-lg-2 control-label" for="Position">Position</label>
-                                    <div class="col-lg-6">
-                                        <select  id='id_title'  name="priority" class="form-control">
-                                            <option selected>Choose a position</option>
-                                            <option>Cashier</option>
-                                            <option>Stock Keeper</option>
-                                            <option>Manager</option>
-                                            <option>Chairman</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="btn btn-default btn-file">
-                                    Upload Photo<input type="file">
-                                </div>
-
-                            <div class="modal-footer">
-                                <!--<button class="btn btn-success" name="submit" value="submit" type="submit">Save</button>-->
-                                <input type="submit" class="btn btn-success" value="Save" name="submit">
-                                <button class="btn btn-default" data-dismiss="modal" type="button">Close</button>
+<div class="row" id="row2">
+    <div class="col-sm-3 feature">
+        <div class="panel">
+            <div class= "panel-heading">
+                <h3 class="panel-title">New Employee</h3>
+            </div>
+            <img src="../../Images/chairman.jpg" alt="HTML5">
+            <div class="col-sm-6">
+                <a href="#addProfile" role="button" class="btn btn-info" data-toggle="modal" id="addButton">ADD PROFILE</a>
+                <div class="modal fade" id="addProfile">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button class ="close" data-dismiss="modal">&times;</button>
+                                <h5 class="modal-title">Add Profile</h5>
                             </div>
 
-                            </form>
+                            <div class="modal-body">
+                                <h6>Employee Details Form</h6><!--mekata data base eka link ka balahan-->
+                                <form class="form-horizontal"  action="SignUpFormProcessing.php" method="post" name= "reg" onsubmit="return checkForm();"  >
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label" for="first_name">First Name</label>
+                                        <div class="col-lg-10">
+                                            <input class="form-control" id="Name" placeholder="First Name" type="text" required name="firstName">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label" for="Last_name">Last Name</label>
+                                        <div class="col-lg-10">
+                                            <input class="form-control" id="Name" placeholder="Lsat Name" type="text" required name="lastName">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label" for="Birth_Day">Birth Day</label>
+                                        <div class="col-lg-10">
+                                            <input class="form-control" type="date" placeholder="Birth Day" name="bDay">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label" for="Username">UserName</label>
+                                        <div class="col-lg-4">
+                                            <input class="form-control" id="Name" placeholder="UserName" type="text" required name="username">
+                                        </div>
+
+                                        <label class="col-lg-2 control-label" for="Password">Password</label>
+                                        <div class="col-lg-4">
+                                            <input class="form-control" id="Age" placeholder="Password" type="password"  required name="passwordName">
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="form-group">
+
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label" for="Confirm_password">Confirm password</label>
+                                        <div class="col-lg-4">
+                                            <input class="form-control" id="Address" placeholder="Confirm password" type="password" required name="confirmPassword">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label" for="Position">Position</label>
+                                        <div class="col-lg-6">
+                                            <select  id='id_title'  name="priority" class="form-control">
+                                                <option selected>Choose a position</option>
+                                                <option>Cashier</option>
+                                                <option>Stock Keeper</option>
+                                                <option>Manager</option>
+                                                <option>Chairman</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="btn btn-default btn-file">
+                                        Upload Photo<input type="file">
+                                    </div>
+
+
+
+                                    <div class="modal-footer" >
+                                        <input type="submit" class="btn btn-success" value="Save" name="submit">
+                                        <button class="btn btn-default" data-dismiss="modal" type="button">Close</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-sm-4 feature">
-            <a class="btn btn-warning btn-info">DELETE PROFILE</a>
-        </div>
-
-        <div class="col-sm-4 feature">
-            <a class="btn btn-warning btn-info">UPDATE PROFILE</a>
-        </div>
     </div>
 </div>
+
+
+
 
 
 <!-- All Javascript at the bottom of the page for faster page loading -->
