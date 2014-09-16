b5f17197cd1f281c43e9ac347a66565e82110af0
Dharmakeerthi Lasantha <tcdlpds@gmail.com>
2014-09-16 19:50:04 +0530

    for stock keeper's purpose

---
+++ Public/PHP/addBook.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once("../PHP_CLASSES/Staff.php");
+require_once("../PHP_CLASSES/StockKeeperClass.php");
+$SANHINDAManager= new StockKeeperClass();
+$SANHINDAManager->formProcessing();
