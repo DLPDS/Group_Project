821e1c51c5e5a220c8dee70b195c8e9261aa5347
Dharmakeerthi Lasantha <tcdlpds@gmail.com>
2014-09-16 19:48:38 +0530

    unwantedfiles

--- Public/PHP/temp.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once("../PHP_CLASSES/Staff.php");
-    require_once("../PHP_CLASSES/ManagerClass.php");
-    $SANHINDAManager= new ManagerClass();
-    //$SANHINDAManager->signInForm();
-    $SANHINDAManager->formProcessing();
-
-?>
